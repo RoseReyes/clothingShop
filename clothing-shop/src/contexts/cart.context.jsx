d60439c9e25f8eb0e0f289f5ad90a3d4f0e1710b
Rose Reyes <roseannlorinreyes@gmail.com>
2023-03-25 20:53:49 -0700

    add sass to cart-item dropdown

--- clothing-shop/src/contexts/cart.context.jsx
+++ clothing-shop/src/contexts/cart.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const addCartItems = (cartItems, productToAdd) => {
   const existCartItem = cartItems.find((cartItem) => cartItem.id === productToAdd.id);
@@ -23,11 +23,17 @@ export const CartContext = createContext({
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([]);
+  const [cartCount, setCartCount] = useState(0);
+
+  useEffect(() => {
+    const newCartCount = cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0)
+    setCartCount(newCartCount);
+  },[cartItems]);
 
   const addItemToCart = (product) => 
     setCartItems(addCartItems(cartItems, product));
   
-  const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart };
+  const value = { isCartOpen, setIsCartOpen, cartItems, cartCount, addItemToCart };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
